fe13c779a05b5dd3bfdfc190deb969cbaf36fc5e
SuryaVtalent <suryaa.vtalent@gmail.com>
2024-08-04 17:45:19 +0530

    One Comment is Added in WebAPIController

--- WebAPICProject/Controllers/EmpWebAPIController.cs
+++ WebAPICProject/Controllers/EmpWebAPIController.cs
@@ -19,6 +19,8 @@ namespace WebAPICProject.Controllers
              EmpRef = _EmpRef;
         }
 
+        //This Method is used to Insert the Records
+
         [HttpPost]
         [Route("InsertEmployee")]
         public async Task<IActionResult> InsertEmployee(Employee Emp)
